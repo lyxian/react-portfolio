663bb7cdaddd4a02d42215c35ecb36c25a0106e5
lyx <yxianlai@hotmail.com>
2022-11-21 02:24:39 +0800

    sticky navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,44 @@
 import React, { Component } from "react";
 import "./App.css";
 
-function Task(props) {
-  const taskStyle = props.isCompleted ? "line-through" : "";
-  return (
-    <div className="todo" style={{ textDecoration: taskStyle }}>
-      {props.text}
-      <div>
-        <button onClick={() => props.completeTask(props.index)}>
-          Complete
-        </button>
-        <button onClick={() => props.removeTask(props.index)}>X</button>
-      </div>
-    </div>
-  );
-}
-
-function TaskForm({ addTask }) {
-  const [value, setValue] = React.useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    addTask(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </form>
-  );
-}
-
 function App() {
-  const [tasks, setTasks] = React.useState([
-    { text: "Learn about React", isCompleted: false },
-    { text: "Meet friend for lunch", isCompleted: false },
-    { text: "Build really cool todo app", isCompleted: false },
-  ]);
-
-  const addTask = (text) => {
-    const newTasks = [...tasks, { text }];
-    setTasks(newTasks);
-  };
-
-  const completeTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks[index].isCompleted = true;
-    setTasks(newTasks);
-  };
-
-  const removeTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
-  };
-
   return (
     <div className="app">
-      <div className="todo-list">
-        {tasks.map((task, index) => (
-          <Task
-            key={index}
-            index={index}
-            text={task.text}
-            isCompleted={task.isCompleted}
-            completeTask={completeTask}
-            removeTask={removeTask}
-          />
-        ))}
-        <TaskForm addTask={addTask} />
-      </div>
+      <header>
+        <img src="https://codetheweb.blog/assets/img/icon2.png" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Terms of use</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consequat
+          libero eget magna commodo, quis pharetra tellus pretium. Sed viverra
+          ante in mauris finibus dapibus. Maecenas congue dapibus nulla, eu
+          gravida orci consequat eu. Phasellus nec nunc malesuada, aliquam massa
+          ac, accumsan metus. Fusce sed dignissim lectus. Nunc elit tellus,
+          sollicitudin ac accumsan ut, egestas et dui. Maecenas aliquam est a
+          ligula scelerisque, in aliquam neque sodales. Nullam condimentum
+          euismod dictum. Curabitur non ex elementum, pretium enim ut, ornare
+          ipsum.
+        </p>
+      </main>
     </div>
   );
 }
